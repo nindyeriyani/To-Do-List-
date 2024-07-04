10d2bced562c81776fbf646a6990d267280a0018
Nindy <macbookpro@nindyeriyani.local>
2024-07-05 03:41:32 +0800

    menambahkan komentar

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if(inputBox.value === '') {
-    alert("You must write something!")
+  if(inputBox.value === '') { // ini mengecek nilai dari inputnya, jika kosong maka akan tampil alert
+    alert("You must write something!") 
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
+    let li = document.createElement("li"); //jika tidak kosong, maka akan dibuat elemen <li>
+    li.innerHTML = inputBox.value; //elemen <li> ini diatur menjadi nilai dari input
+    listContainer.appendChild(li); // elemen <li> akan dimasukkan ke dalam listContainer pakai appendchild()
+    let span = document.createElement("span"); // ini untuk membuat span alias logo x untuk delete
     span.innerHTML = "\u00d7";
-    li.appendChild(span);
+    li.appendChild(span); //memasukkan span kedalam elemen <li>
   }
-  inputBox.value = "";
-  saveData();
+  inputBox.value = ""; //membersihkan nilai input box, supaya bisa digunakan lagi untuk menginput to do selanjutnya
+  saveData(); //menyinpan data ke localstorage
 }
 
-listContainer.addEventListener("click", function(e) {
+listContainer.addEventListener("click", function(e) { //setiap kali diklik, maka akan dijalankan fungsi di dalamnya 
   if(e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
     saveData();
@@ -35,3 +35,7 @@ function showTask() {
 }
 
 showTask();
+
+
+//function(e) = fungsi callback yang akan dieksekusi setiap kali event "click" terjadi pada elemen target.
+//e  Merupakan parameter yang mewakili objek event, yang digunakan untuk mengakses informasi terkait event klik tersebut.
